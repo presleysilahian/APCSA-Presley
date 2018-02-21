8c3778cdc5c5f80a80e570c43bdccc81db6ee1a7
Presley Silahian <presleyjake11@gmail.com>
2018-02-21 10:15:29 -0800

    Updates

--- Unit 7/src/Triples.java
+++ Unit 7/src/Triples.java
@@ -17,29 +17,42 @@ public class Triples
 
 	public Triples(int num)
 	{
-
+		setNum(num);
 
 	}
 
 	public void setNum(int num)
 	{
-
+		number = num;
 
 	}
 	
 	private int greatestCommonFactor(int a, int b, int c)
 	{
-		int max = 0;
+		int max = 1;
+		for (int i = 1; i <= a; i++){
+			if (a % i == 0 && b%i == 0 && c%i == 0 && i > max){
+				max = i;
+			}
+			
+		}
 
 
-
-		return 1;
+		return max;
 	}
 
 	public String toString()
 	{
 		String output="";
-
+		for (int i = 1; i<=number; i++){
+			for (int j = i+1; j <= number; j++){
+				for (int k = j+1; k <= number; k++){
+					if (i*i + j*j == k*k && greatestCommonFactor(i,j,k) == 1){
+						output += " " + i + " " + j + " " + k + " " + "\n";
+					}
+				}
+			}
+		}
 
 
 
