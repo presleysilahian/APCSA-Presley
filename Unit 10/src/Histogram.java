db85b5b72636e9448753dc5873320d832d1420d1
Presley Silahian <presleyjake11@gmail.com>
2018-03-09 11:01:01 -0800

    SuhWOOOO

--- Unit 10/src/Histogram.java
+++ Unit 10/src/Histogram.java
@@ -27,38 +27,67 @@ public class Histogram
 
 	public Histogram(char[] values, String fName)
 	{
-
-
-
-
-
-
+		count = new ArrayList<Integer>();
+		letters = new ArrayList<Character>();
+		fileName = fName;
+		for(int i = 0; i < values.length; i++){
+			letters.add(values[i]);
+			count.add(0);
+		}
 		out.println("search letters = "+letters);
+		
 	}
 
 	public void loadAndAnalyzeFile() throws IOException
 	{
-
-
-
-
-
-
-
-
+		try{
+			Scanner scan = new Scanner(new File(fileName));
+			int numLines = scan.nextInt();
+			scan.nextLine();
+			for(int i = 0; i < numLines; i++){
+				String ln = scan.nextLine();
+				for(int j = 0; j < letters.size(); j++){
+					for(int k = 0; k < ln.length(); k++){
+						if(ln.charAt(k) == letters.get(j)){
+							count.set(j, count.get(j) + 1);
+						}
+					}
+				}
+			}
+				
+			
+		}
+		catch(Exception e){
+			System.out.println("Error");
+		}
 	}
 
 	public char mostFrequent()
 	{
-
-		return '#';
+		int a = 0;
+		char c = letters.get(0);
+		for(int i = 0; i < letters.size(); i++){
+			if(count.get(i) > a){
+				c = letters.get(i);
+				a = count.get(i);
+			}
+		}
+		return c;
+		
 	}
 
 	public char leastFrequent()
 	{
-
-
-		return '#';
+		int a = Integer.MAX_VALUE;
+		char c = letters.get(0);
+		for(int i = 0; i < letters.size(); i++){
+			if(count.get(i) < a){
+				c = letters.get(i);
+				a = count.get(i);
+			}
+		}
+		return c;
+		
 	}
 
 	public String toString()
--- Unit 10/src/Lab16e.java
+++ Unit 10/src/Lab16e.java
@@ -15,14 +15,28 @@ public class Lab16e
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Histogram test = new Histogram(new char[]{'a','b','c'},"lab16e1.dat");
-		test.loadAndAnalyzeFile();
-		out.println("Letter occurring most : "+test.mostFrequent());
-		out.println("Letter occurring least : "+test.leastFrequent());
+		Histogram test1 = new Histogram(new char[]{'a','b','c'},"C:\\Users\\silahianp1916\\Desktop\\APCSA-Presley\\Unit 10\\src\\lab16e1.dat");
+		test1.loadAndAnalyzeFile();
+		out.println("Letter occurring most : "+test1.mostFrequent());
+		out.println("Letter occurring least : "+test1.leastFrequent());
 		out.println("\n");
-		out.println(test);
+		out.println(test1);
+		
+		Histogram test2 = new Histogram(new char[]{'d','e','a'},"C:\\Users\\silahianp1916\\Desktop\\APCSA-Presley\\Unit 10\\src\\lab16e2.dat");
+		test2.loadAndAnalyzeFile();
+		out.println("Letter occurring most : "+test2.mostFrequent());
+		out.println("Letter occurring least : "+test2.leastFrequent());
+		out.println("\n");
+		out.println(test2);
+		
+		Histogram test3 = new Histogram(new char[]{'x','a','r','g'},"C:\\Users\\silahianp1916\\Desktop\\APCSA-Presley\\Unit 10\\src\\lab16e3.dat");
+		test3.loadAndAnalyzeFile();
+		out.println("Letter occurring most : "+test3.mostFrequent());
+		out.println("Letter occurring least : "+test3.leastFrequent());
+		out.println("\n");
+		out.println(test3);
 
 
-		//more test cases
+	
 	}
 }
