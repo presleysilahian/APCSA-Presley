c334baa944fca072cc46c3bfff1bc1696adb33a2
Presley Silahian <presleyjake11@gmail.com>
2018-03-12 11:12:11 -0700

    Updates

---
+++ Unit 11/src/WordPrinter.java
@@ -0,0 +1,26 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import static java.lang.System.*;
+
+public class WordPrinter
+{
+	public WordPrinter(String word, int times)
+	{
+		printWord(word, times);
+		
+	}
+		
+	public static void printWord(String word, int times)
+	{
+		for (int i = 0; i < times; i++){
+			System.out.println(word);
+		}
+
+
+
+	}
+}
