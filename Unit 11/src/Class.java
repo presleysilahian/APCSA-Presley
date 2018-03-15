134699079e3725b6355f4509bc6a922d84eb59e2
Presley Silahian <presleyjake11@gmail.com>
2018-03-15 10:47:23 -0700

    Unit 11

--- Unit 11/src/Class.java
+++ Unit 11/src/Class.java
@@ -5,6 +5,8 @@
 //Lab  -
 
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 import static java.lang.System.*;
 import static java.util.Arrays.*;
@@ -12,23 +14,28 @@ import static java.util.Arrays.*;
 public class Class
 {
 	private String name;
-	private Student[] studentList;
+	private ArrayList<Student> studentList;
 	
 	public Class()
 	{
 		name="";
-		studentList=new Student[0];
+		studentList=new ArrayList<Student>();
 	}
 	
-	public Class(String name, int stuCount)
+	public Class(String name1, int stuCount)
 	{
-		this.name = name;
-		studentList = new Student[stuCount];
+		name = name1;
+		studentList = new ArrayList<Student>();
+		
+
+	
 	}
 	
 	public void addStudent(int stuNum, Student s)
 	{
-		studentList[stuNum] = s;
+		studentList.add(stuNum, s);
+
+
 	}
 	
 	public String getClassName()
@@ -38,25 +45,26 @@ public class Class
 	
 	public double getClassAverage()
 	{
+		
 		double classAverage=0.0;
-		for (int i = 0; i < studentList.length; i++){
-			classAverage+= studentList[i].getAverage();
-			
+		for (int i = 0; i<studentList.size();i++){
+			classAverage += studentList.get(i).getAverage();
 		}
-		classAverage = classAverage/studentList.length;
+		classAverage /= studentList.size();
 		return classAverage;
 	}
 	
 	public double getStudentAverage(int stuNum)
 	{
-		return studentList[stuNum].getAverage();
+		
+		return studentList.get(stuNum).getAverage();
 	}
 
 	public double getStudentAverage(String stuName)
 	{
 		int index = -1;
-		for (int i = 0; i < studentList.length; i++){
-			if (studentList[i].getName().equals(stuName)){
+		for (int i = 0; i<studentList.size();i++){
+			if (studentList.get(i).getName().equals(stuName)){
 				index = i;
 			}
 		}
@@ -65,55 +73,82 @@ public class Class
 	
 	public String getStudentName(int stuNum)
 	{
-		return studentList[stuNum].getName();
+		
+		return studentList.get(stuNum).getName() ;
 	}
 
 	public String getStudentWithHighestAverage()
 	{
-		double high = Double.MIN_VALUE;
-		String hName = "";
-		high = studentList[0].getAverage();
-		for (int i = 0; i < studentList.length; i++){
-			if (studentList[i].getAverage() > high){
+		/**double high = Double.MIN_VALUE;
+		String hName ="";
+		for(int i= 0; i<studentList.length;i++){
+			if(studentList[i].getAverage()> high){
 				high = studentList[i].getAverage();
 				hName = studentList[i].getName();
 			}
 		}
-		return hName;
+		return hName;**/
+		
+		int index = 0;
+		for(int i= 0; i<studentList.size()-1;i++){
+			if(studentList.get(index).compareTo(studentList.get(i+1)) == -1){
+				index = i + 1;
+			}
+		}
+		return studentList.get(index).getName();
 	}
 
 	public String getStudentWithLowestAverage()
 	{
-		double low = Double.MAX_VALUE;
-		String hName ="";		
-		low = studentList[0].getAverage();
-		for (int i = 0; i < studentList.length; i++){
-			if (studentList[i].getAverage() < low){
+		/*double low = Double.MAX_VALUE;
+		String hName ="";
+		for(int i= 0; i<studentList.length;i++){
+			if(studentList[i].getAverage()< low){
 				low = studentList[i].getAverage();
 				hName = studentList[i].getName();
 			}
 		}
-		return hName;
+		return hName;*/
+		
+		int index = 0;
+		for(int i= 0; i<studentList.size()-1;i++){
+			if(studentList.get(index).compareTo(studentList.get(i+1)) == 1){
+				index  = i + 1;
+			}
+		}
+		return studentList.get(index).getName();
+
 	}
 	
 	public String getFailureList(double failingGrade)
 	{
 		String output="";
-		for (int i = 0; i < studentList.length; i++){
-			if (studentList[i].getAverage() < failingGrade){
-				output+= studentList[i].getName() + " ";
+		for (int i = 0; i<studentList.size();i++){
+			if(studentList.get(i).getAverage()<failingGrade){
+				output+= studentList.get(i).getName() + " ";
 			}
 		}
-		
+
 		return output;
+	}
+	
+	public void sort(){
+		Collections.sort(studentList);
+		
 	}
 	
 	public String toString()
 	{
 		String output=""+getClassName()+"\n";
-		for (int i = 0; i < studentList.length; i++){
-			output+= studentList[i] + "  " + studentList[i].getAverage() + "\n";
+		for (int i = 0; i<studentList.size();i++){
+			output += studentList.get(i) + "  " + studentList.get(i).getAverage()+ "\n"; 
 		}
+		
+
+
+
 		return output;
 	}  	
 }
+
+
