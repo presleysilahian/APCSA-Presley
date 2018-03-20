694ae5adb96bd5d45826e26f19f2c33baf259dbd
Presley Silahian <36010613+presleysilahian@users.noreply.github.com>
2018-03-19 18:41:44 -0700

    Add files via upload

--- Unit 12/src/Lab18e.java
+++ Unit 12/src/Lab18e.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -15,40 +15,24 @@ public class Lab18e
 {
 	public static void main( String args[] ) throws IOException
 	{
-		ArrayList<String> stuff = new ArrayList<String>();
-		Scanner scanny = new Scanner(new File("C:\\Users\\silahianp1916\\Desktop\\APCSA-Presley\\Unit 12\\src\\lab18e.dat"));
-		int num = scanny.nextInt();
-		scanny.nextLine();
-		while (scanny.hasNextLine())
-		{
-			stuff.add(scanny.nextLine());
-		}
+		Scanner file = new Scanner(new File("C:\\Users\\silahianp1916\\Desktop\\APCSA-Presley\\Unit 12\\src\\lab18e.dat"));
+		int size = file.nextInt();
+		file.nextLine();
+		Wordy[] list = new Wordy[size];
+		for(int i = 0; i < size; i++) {
+			list[i] = new Wordy(file.next());
+		} file.close();
 		
-		for (int i = 0; i < num; i++){
-			Wordy papa = new Wordy(stuff.get(i));
-			for (int j = 0; j <i; j++){
-				Wordy mother = new Wordy(stuff.get(j));
-				if (papa.compareTo(mother)==1){
-					String wank = papa.toString();
-					String dank = mother.toString();
-					stuff.set(i, mother.toString());
-					stuff.set(j,  papa.toString());
+		for(int i = 0; i < list.length; i++) {
+			for(int j = 0; j < list.length-1; j++) {
+				if(list[j].compareTo(list[j+1]) == -1) {
+					Wordy t = list[j];
+					list[j] = list[j+1];
+					list[j+1] = t;
 				}
 			}
 		}
-		for (int i = 0; i < stuff.size(); i++){
-			out.println(stuff.get(i));
-		}
-
-
-
-
-
-
-
-
-
-
-
+				
+		for(Wordy w : list) out.println(w);			
 	}
 }
