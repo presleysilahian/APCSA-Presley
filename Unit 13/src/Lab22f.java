a9fb9a9b3ddbfc0debe4681aa9a255582e923c1e
Presley Silahian <presleyjake11@gmail.com>
2018-03-29 10:52:35 -0700

    tc

--- Unit 13/src/Lab22f.java
+++ Unit 13/src/Lab22f.java
@@ -9,12 +9,15 @@ public class Lab22f
 	public static void main(String args[])
 	{
 		QuickSort.quickSort(new Comparable[]{9,5,3,2});
+		System.out.println("\n");
 
-
+		QuickSort.zeroPassCount();
 		QuickSort.quickSort(new Comparable[]{19,52,3,2,7,21});
+		System.out.println("\n");
 
-
+		QuickSort.zeroPassCount();
 		QuickSort.quickSort(new Comparable[]{68,66,11,2,42,31});
+		
 	}
 }
 
--- Unit 13/src/QuickSort.java
+++ Unit 13/src/QuickSort.java
@@ -5,49 +5,44 @@
 //Lab  - 
 
 import static java.lang.System.*;
-import java.util.Arrays;		//use Arrays.toString() to help print out the array
+import java.util.Arrays;	
 
 public class QuickSort
 {
 	private static int passCount;
 
+	public static void zeroPassCount() {passCount = 0;}
+	
 	public static void quickSort(Comparable[] list)
 	{
-
-
-
-
+		quickSort(list, 0, list.length-1);
 	}
 
 
 	private static void quickSort(Comparable[] list, int low, int high)
 	{
-
-
-
-
-
-
-
+		if(low < high) {
+			int split = partition(list, low, high);
+			out.println("pass " + passCount++ + " " + Arrays.toString(list));
+			quickSort(list, low, split);
+			quickSort(list, split+1, high);
+		}
 	}
 
 
 	private static int partition(Comparable[] list, int low, int high)
 	{
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		return 0;
+		Comparable pivot = list[low];
+		int bot = low-1;
+		int top = high+1;
+		while(bot < top) {
+			while(list[--top].compareTo(pivot) > 0);
+			while(list[++bot].compareTo(pivot) < 0);
+			if(bot>=top)
+				return top;
+			Comparable temp = list[bot];
+			list[bot] = list[top];
+			list[top] = temp;
+		} return 0;
 	}
 }
