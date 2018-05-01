ca62936d653ef2902ec676d9935fe842e310f83c
Presley Silahian <presleyjake11@gmail.com>
2018-05-01 10:54:56 -0700

    BIG updates

--- Unit 17/src/OuterSpace.java
+++ Unit 17/src/OuterSpace.java
@@ -1,3 +1,4 @@
+
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -15,27 +16,27 @@ import static java.lang.Character.*;
 import java.awt.image.BufferedImage;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
 @SuppressWarnings("serial")
-public class OuterSpace extends Canvas implements KeyListener, Runnable
-{
+public class OuterSpace extends Canvas implements KeyListener, Runnable {
 	private Ship ship;
 	private Bullets shots;
-	private int	tick;
+	private int tick;
 	private AlienHorde horde;
 	private Alien alienOne;
 	private Alien alienTwo;
+	private Shield shield;
 
-	/* uncomment once you are ready for this part
+	/*
+	 * uncomment once you are ready for this part
 	 *
-   private AlienHorde horde;
-	private Bullets shots;
-	*/
+	 * private AlienHorde horde; private Bullets shots;
+	 */
 
 	private boolean[] keys;
 	private BufferedImage back;
 
-	public OuterSpace()
-	{
+	public OuterSpace() {
 		setBackground(Color.black);
 
 		keys = new boolean[5];
@@ -48,11 +49,11 @@ public class OuterSpace extends Canvas implements KeyListener, Runnable
 
 		horde = new AlienHorde(hordeSize);
 		shots = new Bullets();
+		shield = new Shield();
 
 		for (int x = 8; x < StarFighter.WIDTH - 100; x += (StarFighter.WIDTH) / 8)
 			for (int y = 22; y < StarFighter.HEIGHT / 1.5; y += (StarFighter.HEIGHT / 1.5) / 4)
 				horde.add(new Alien(x + 20, y, 25, 25, 1));
-
 
 		this.addKeyListener(this);
 		new Thread(this).start();
@@ -60,34 +61,32 @@ public class OuterSpace extends Canvas implements KeyListener, Runnable
 		setVisible(true);
 	}
 
-   public void update(Graphics window)
-   {
-	   paint(window);
-   }
+	public void update(Graphics window) {
+		paint(window);
+	}
 
-	public void paint( Graphics window )
-	{
-		//set up the double buffering to make the game animation nice and smooth
-		Graphics2D twoDGraph = (Graphics2D)window;
+	public void paint(Graphics window) {
+		// set up the double buffering to make the game animation nice and
+		// smooth
+		Graphics2D twoDGraph = (Graphics2D) window;
 
-		//take a snap shop of the current screen and same it as an image
-		//that is the exact same width and height as the current screen
-		if(back==null)
-		   back = (BufferedImage)(createImage(getWidth(),getHeight()));
+		// take a snap shop of the current screen and same it as an image
+		// that is the exact same width and height as the current screen
+		if (back == null)
+			back = (BufferedImage) (createImage(getWidth(), getHeight()));
 
-		//create a graphics reference to the back ground image
-		//we will draw all changes on the background image
+		// create a graphics reference to the back ground image
+		// we will draw all changes on the background image
 		Graphics graphToBack = back.createGraphics();
 
 		graphToBack.setColor(Color.BLUE);
-		graphToBack.drawString("StarFighter ", 25, 50 );
+		graphToBack.drawString("StarFighter ", 25, 50);
 		graphToBack.setColor(Color.BLACK);
-		graphToBack.fillRect(0,0,800,600);
+		graphToBack.fillRect(0, 0, 800, 600);
 
 		tick++;
-		
-		if(keys[0] == true)
-		{
+
+		if (keys[0] == true) {
 			ship.move("LEFT");
 		}
 
@@ -115,12 +114,14 @@ public class OuterSpace extends Canvas implements KeyListener, Runnable
 					shots.getAmmo().remove(i--);
 					horde.getAliens().remove(j--);
 					horde.decSize();
-					
+
 				}
 		for (int i = 0; i < horde.getAliens().size(); i++)
 			if (ship.isColliding(horde.getAliens().get(i))) {
-				System.out.println("You lose.");
-				System.exit(0);
+				if (horde.getSize() > 18 || horde.getSize() <= 14) {
+					System.out.println("You lose.");
+					System.exit(0);
+				}
 			}
 		horde.moveEmAll();
 		shots.moveEmAll();
@@ -134,80 +135,62 @@ public class OuterSpace extends Canvas implements KeyListener, Runnable
 		graphToBack.setColor(Color.WHITE);
 		graphToBack.drawString("" + horde.getSize(), 740, 530);
 		ship.draw(graphToBack);
+		shield.draw(graphToBack, ship, horde);
 		shots.drawEmAll(graphToBack);
 		horde.drawEmAll(graphToBack);
 
 		twoDGraph.drawImage(back, null, 0, 0);
 	}
 
-
-	public void keyPressed(KeyEvent e)
-	{
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-		{
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 			keys[0] = true;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			keys[1] = true;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_UP)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
 			keys[2] = true;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_DOWN)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 			keys[3] = true;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_SPACE)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 			keys[4] = true;
 		}
 		repaint();
 	}
 
-	public void keyReleased(KeyEvent e)
-	{
-		if (e.getKeyCode() == KeyEvent.VK_LEFT)
-		{
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 			keys[0] = false;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			keys[1] = false;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_UP)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
 			keys[2] = false;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_DOWN)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 			keys[3] = false;
 		}
-		if (e.getKeyCode() == KeyEvent.VK_SPACE)
-		{
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
 			keys[4] = false;
 		}
 		repaint();
 	}
 
-	public void keyTyped(KeyEvent e)
-	{
-      //no code needed here
+	public void keyTyped(KeyEvent e) {
+		// no code needed here
 	}
 
-   public void run()
-   {
-   	try
-   	{
-   		while(true)
-   		{
-   		   Thread.currentThread().sleep(5);
-            repaint();
-         }
-      }catch(Exception e)
-      {
-      }
-  	}
+	public void run() {
+		try {
+			while (true) {
+				Thread.currentThread().sleep(5);
+				repaint();
+			}
+		} catch (Exception e) {
+		}
+	}
 }
-
